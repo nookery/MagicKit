04206e6577111744abaec9520033ae0e509ebb43
nookery <nooks@qq.com>
2026-01-26 11:56:51 +0800

    refactor(url): simplify open button implementation
    Replace complex HStack structure with single Image view using
    system icons. Improve visual styling with card effect, hover
    animation, and shadow.
    
    Changes:
    - Use appType.icon(for:) instead of magicButtonIcon
    - Add inCard(.regularMaterial) for background
    - Add hoverScale(105) for interactivity
    - Add shadowSm() and roundedFull() for styling
    - Remove commented-out displayName code

--- Sources/MagicKit/URL/Open/Open+Button.swift
+++ Sources/MagicKit/URL/Open/Open+Button.swift
@@ -8,20 +8,22 @@ public extension URL {
     ///   - useRealIcon: 是否使用真实应用图标（仅macOS），默认为false使用系统图标
     /// - Returns: 打开按钮视图
     func makeOpenButton(_ appType: OpenAppType = .auto, useRealIcon: Bool = false) -> some View {
-        HStack {
-//            appType.magicButtonIcon(for: self, useRealIcon: useRealIcon)
-//            appType.displayName(for: self)
-        }.inButtonWithAction({
-            #if os(macOS)
-                openIn(appType)
-            #else
-                if appType == .auto {
-                    open()
-                } else {
-                    open() // iOS上所有类型都使用默认打开方式
-                }
-            #endif
-        })
+        Image(systemName: appType.icon(for: self))
+            .inButtonWithAction({
+                #if os(macOS)
+                    openIn(appType)
+                #else
+                    if appType == .auto {
+                        open()
+                    } else {
+                        open() // iOS上所有类型都使用默认打开方式
+                    }
+                #endif
+            })
+            .inCard(.regularMaterial)
+            .hoverScale(105)
+            .shadowSm()
+            .roundedFull()
     }
 }
 
