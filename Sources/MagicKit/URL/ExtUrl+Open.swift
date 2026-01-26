ceff38456304480cba73325bd4ee43293f20cb7f
nookery <nooks@qq.com>
2026-01-26 18:22:40 +0800

    chore:  minor adjustments

--- Sources/MagicKit/URL/ExtUrl+Open.swift
+++ Sources/MagicKit/URL/ExtUrl+Open.swift
@@ -8,6 +8,30 @@ import SwiftUI
 #endif
 
 public extension URL {
+    /// 创建打开按钮
+    /// - Parameters:
+    ///   - appType: 应用程序类型，默认为 .auto（智能选择）
+    ///   - useRealIcon: 是否使用真实应用图标（仅macOS），默认为false使用系统图标
+    /// - Returns: 打开按钮视图
+    func makeOpenButton(_ appType: OpenAppType = .auto, useRealIcon: Bool = false) -> some View {
+        Image(systemName: appType.icon(for: self))
+            .inButtonWithAction({
+                #if os(macOS)
+                    openIn(appType)
+                #else
+                    if appType == .auto {
+                        open()
+                    } else {
+                        open() // iOS上所有类型都使用默认打开方式
+                    }
+                #endif
+            })
+            .inCard(.regularMaterial)
+            .hoverScale(105)
+            .shadowSm()
+            .roundedFull()
+    }
+
     /// 打开URL：如果是网络链接则在浏览器打开，如果是本地文件则在访达中显示
     func open() {
         if isNetworkURL {
@@ -48,7 +72,7 @@ public extension URL {
                 open()
                 return
             }
-            
+
             if appType == .browser {
                 openInBrowser()
                 return
@@ -90,8 +114,7 @@ public extension URL {
 }
 
 #if DEBUG
-#Preview("Open Buttons") {
-    OpenPreivewView()
-        
-}
+    #Preview("Open Buttons") {
+        OpenPreivewView()
+    }
 #endif
